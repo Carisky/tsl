8ca915ad114c6cce0394cfb9a6d1d480a3de6c77
Yehor <egorkorsungit@gmail.com>
2025-03-13 11:47:31 +0100

    made nav responsive for users with diff screens

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -1,21 +1,29 @@
 'use client'
 
 import React from 'react'
-
 import type { Header as HeaderType } from '@/payload-types'
-
 import { CMSLink } from '@/components/Link'
 import Link from 'next/link'
 import { SearchIcon } from 'lucide-react'
 
-export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
+interface HeaderNavProps {
+  data: HeaderType
+  vertical?: boolean
+}
+
+export const HeaderNav: React.FC<HeaderNavProps> = ({ data, vertical }) => {
   const navItems = data?.navItems || []
+  
+  // Если vertical=true, располагаем элементы вертикально, иначе горизонтально.
+  const navClassName = vertical 
+    ? "flex flex-col gap-4" 
+    : "flex gap-3 items-center"
 
   return (
-    <nav className="flex gap-3 items-center">
-      {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />
-      })}
+    <nav className={navClassName}>
+      {navItems.map(({ link }, i) => (
+        <CMSLink key={i} {...link} appearance="link" />
+      ))}
       <Link href="/search">
         <span className="sr-only">Search</span>
         <SearchIcon className="w-5 text-primary" />
