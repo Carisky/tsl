5aa8aa789016e99199544a02024351b467dab598
Yehor <egorkorsungit@gmail.com>
2025-03-14 13:36:24 +0100

    hero fix

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -16,13 +16,22 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
   })
 
   return (
-    <div
-      className="relative -mt-[10.4rem] flex items-center justify-center text-white"
-      data-theme="dark"
-    >
+    <div className="relative -mt-[10.4rem] flex items-center justify-center" data-theme="dark">
       <div className="container mb-8 z-10 relative flex items-center justify-center">
-        <div className="max-w-[36.5rem] md:text-center">
-          {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
+        <div
+          className="pb-6 pt-6 pl-6 pr-6 max-w-[36.5rem] bg-[#010101]"
+          style={{
+            clipPath:
+              "polygon(0% 0%, 100% 20%, 100% 94.5%, 0% 75.43%)",
+          }}   
+        >
+          {richText && (
+            <RichText
+              className="text-white flex items-center justify-center"
+              data={richText}
+              enableGutter={false}
+            />
+          )}
           {Array.isArray(links) && links.length > 0 && (
             <ul className="flex md:justify-center gap-4">
               {links.map(({ link }, i) => {
