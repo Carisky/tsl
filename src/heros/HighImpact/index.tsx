59cbf3f63a49851536af2beef62b6e703bc1a2b3
Yehor <egorkorsungit@gmail.com>
2025-03-18 11:23:03 +0100

    paddings for fit content

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -19,7 +19,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
     <div className="relative -mt-[10.4rem] flex items-center justify-center" data-theme="dark">
       <div className="container mb-8 z-10 relative flex items-center justify-center">
         <div
-          className="pb-6 pt-6 pl-6 pr-6 max-w-[36.5rem] bg-[#010101]"
+          className="pb-16 pt-16 pl-16 pr-16 max-w-[36.5rem] bg-[#010101]"
           style={{
             clipPath:
               "polygon(0% 0%, 100% 20%, 100% 94.5%, 0% 75.43%)",
