9931fa1a9265b7e429a80e7417c5bd97de3ea948
Yehor <egorkorsungit@gmail.com>
2025-03-24 10:51:31 +0100

    Icons List switched to mui and mui icons

--- src/blocks/IconsList/IconsListBlock.tsx
+++ src/blocks/IconsList/IconsListBlock.tsx
@@ -1,12 +1,5 @@
 import React from 'react'
-import {
-  Box,
-  Typography,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText
-} from '@mui/material'
+import { Box, Typography, List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import InfoIcon from '@mui/icons-material/Info'
@@ -28,27 +21,30 @@ export const IconsListBlock: React.FC<IconsListBlockProps> = ({ title, items }) 
   if (!items || items.length === 0) return null
 
   const iconMapping: Record<string, React.ReactNode> = {
-    ChevronRight: <ChevronRightIcon sx={{ color: "#8d004c" }} />,
-    ChevronLeft: <ChevronLeftIcon sx={{ color: "#8d004c" }} />,
-    Info: <InfoIcon sx={{ color: "#8d004c" }} />,
-    Alert: <ErrorOutlineIcon sx={{ color: "#8d004c" }} />,
-    Check: <CheckIcon sx={{ color: "#8d004c" }} />
+    ChevronRight: <ChevronRightIcon sx={{ color: '#8d004c' }} />,
+    ChevronLeft: <ChevronLeftIcon sx={{ color: '#8d004c' }} />,
+    Info: <InfoIcon sx={{ color: '#8d004c' }} />,
+    Alert: <ErrorOutlineIcon sx={{ color: '#8d004c' }} />,
+    Check: <CheckIcon sx={{ color: '#8d004c' }} />,
   }
 
   return (
-    <Box sx={{ maxWidth:"86vw", margin:"auto"}}>
+    <Box className="container">
       {title && (
-        <Typography variant="h4" color="primary" sx={{ mb: 2 }}>
+        <Typography variant="h4" color="primary" sx={{ mb: 2, fontWeight: '600' }}>
           {title}
         </Typography>
       )}
       <List>
         {items.map((item) => (
           <ListItem key={item.id}>
-            <ListItemIcon>
-              {iconMapping[item.icon] || item.icon}
-            </ListItemIcon>
-            <ListItemText primary={item.text} />
+            <ListItemIcon>{iconMapping[item.icon] || item.icon}</ListItemIcon>
+            <ListItemText
+              primary={item.text}
+              slotProps={{
+                primary: { sx: { fontSize: '1rem' } },
+              }}
+            />
           </ListItem>
         ))}
       </List>
