9f51441c92b864de24ad8e9fa5c95e852e07446b
Yehor <egorkorsungit@gmail.com>
2025-03-25 11:06:16 +0100

    header mobile view fix

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -16,7 +16,7 @@ export const HeaderNav: React.FC<HeaderNavProps> = ({ data, vertical }) => {
   
   // Если vertical=true, располагаем элементы вертикально, иначе горизонтально.
   const navClassName = vertical 
-    ? "flex flex-col gap-4 text-black" 
+    ? "flex flex-col gap-4 w-40 text-black" 
     : "flex gap-3 items-center h-20 text-white" 
   const linkColor = vertical ? "text-black" : "text-white"
   return (
