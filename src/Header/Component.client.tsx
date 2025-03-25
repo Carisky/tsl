09195a6704abbf1e69a08c754f3491154b8a3f4d
Yehor <egorkorsungit@gmail.com>
2025-03-25 11:07:41 +0100

    header visual fix

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -10,6 +10,7 @@ import { IoMdMenu, IoMdClose } from 'react-icons/io'
 import { useLocaleStore } from '@/app/(frontend)/store/useLocaleStore'
 import LanguageSwitcher from '@/app/(frontend)/components/LanguageSwitcher'
 import palette from '@/palette'
+import { Box } from '@mui/material'
 
 interface HeaderClientProps {
   data: Header
@@ -47,20 +48,20 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
       {...(theme ? { 'data-theme': theme } : {})}
       style={{ backgroundColor: palette.nav.background}}
     >
-      <div className="flex items-center justify-between">
+      <Box className="flex items-center justify-between">
         <Link href="/" className="flex-shrink-0 min-w-[80px]">
           <Logo loading="eager" priority="high" className="invert dark:invert-0 w-20 h-20" />
         </Link>
 
-        <div className="flex items-center">
+        <Box className="flex items-center">
           {/* Десктопное меню */}
-          <div className="hidden xl:flex items-center space-x-4">
+          <Box className="hidden xl:flex items-center space-x-4">
             <LanguageSwitcher />
             <HeaderNav data={data} />
-          </div>
+          </Box>
 
           {/* Бургер-иконка для мобильных */}
-          <div className="xl:hidden">
+          <Box className="xl:hidden">
             <button
               onClick={toggleMobileMenu}
               className="p-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -71,27 +72,30 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
                 <IoMdMenu className="h-6 w-6" />
               )}
             </button>
-          </div>
-        </div>
-      </div>
+          </Box>
+        </Box>
+      </Box>
 
       {/* Модальное правое меню (sidebar) для мобильных */}
       {isMobileMenuOpen && (
-        <div className="fixed inset-0 z-30 flex">
+        <Box className="fixed inset-0 z-30 flex">
           {/* Затемненная область слева для закрытия */}
-          <div className="flex-1" onClick={toggleMobileMenu}></div>
+          
+          <Box className="flex-1" onClick={toggleMobileMenu}></Box>
+          
           {/* Sidebar меню */}
-          <div className="w-[55%] h-full bg-white shadow-lg p-6 flex flex-col">
-            <div className="flex justify-end">
+          <Box className="max-w-sm h-full bg-white shadow-lg p-6 flex flex-col">
+            <Box className="flex justify-end">
+              
               <button
                 onClick={toggleMobileMenu}
                 className="p-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
               >
                 <IoMdClose className="h-6 w-6" />
               </button>
-            </div>
+            </Box>
             {/* Переключатель языка */}
-            <div className="flex justify-around my-4">
+            <Box className="flex justify-around my-4">
               <button
                 onClick={() => handleLanguage('en')}
                 className="px-3 py-2 border rounded hover:bg-blue-500 hover:text-white transition"
@@ -116,13 +120,14 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
               >
                 🇵🇱
               </button>
-            </div>
+
+            </Box>
             {/* Вертикальный список навигации */}
-            <div className="flex flex-col space-y-4 mt-4">
+            <Box className="flex flex-col space-y-4 mt-4">
               <HeaderNav data={data} vertical={true} />
-            </div>
-          </div>
-        </div>
+            </Box>
+          </Box>
+        </Box>
       )}
     </header>
   )
