a43d2c5691407f2a77a028b72fb5e281639c4ba8
Yehor <egorkorsungit@gmail.com>
2025-03-26 09:28:34 +0100

    added locale save to cookies array

--- src/app/(frontend)/store/useLocaleStore.ts
+++ src/app/(frontend)/store/useLocaleStore.ts
@@ -1,16 +1,20 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import Cookies from "js-cookie"; // установите через npm install js-cookie
 
 interface LocaleState {
-  locale: string |undefined;
+  locale: string | undefined;
   setLocale: (locale: string) => void;
 }
 
 export const useLocaleStore = create<LocaleState>()(
   persist(
     (set) => ({
-      locale: "pl",
-      setLocale: (locale) => set({ locale }),
+      locale: Cookies.get("locale") || "pl",
+      setLocale: (locale: string) => {
+        Cookies.set("locale", locale, { expires: 365 }); // сохраняем cookie на год
+        set({ locale });
+      },
     }),
     { name: "locale-storage" }
   )
