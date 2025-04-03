dad26d4c473e5e7b91f2bf672fc22471596bb3df
Yehor <egorkorsungit@gmail.com>
2025-04-03 14:23:48 +0200

    grid view fix for image collections

--- src/blocks/ImageSlider/Component.tsx
+++ src/blocks/ImageSlider/Component.tsx
@@ -124,7 +124,7 @@ export default function ImageSlider({
                   width: '100%',
                   height: '100%',
                   aspectRatio:"1/1",
-                  objectFit: 'cover',
+                  objectFit: "fill",
                 }}
               />
             </Box>
