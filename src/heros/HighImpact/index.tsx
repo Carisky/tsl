31770f220d59cf1d3a1f31ef7a826eafe80a88ad
Yehor <egorkorsungit@gmail.com>
2025-04-10 09:01:59 +0200

    cahnged HighImpactHero render

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -7,8 +7,14 @@ import type { Page } from '@/payload-types'
 import { CMSLink } from '@/components/Link'
 import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
+import { Box } from '@mui/material'
 
-export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
+export const HighImpactHero: React.FC<Page['hero']> = ({
+  links,
+  media,
+  richText,
+  showRichText,
+}) => {
   const { setHeaderTheme } = useHeaderTheme()
 
   useEffect(() => {
@@ -18,18 +24,24 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
   return (
     <div className="relative -mt-[10.4rem] flex items-center justify-center" data-theme="dark">
       <div className="container mb-8 z-10 relative flex items-center justify-center">
-
-          {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex flex-wrap justify-center gap-4">
-              {links.map(({ link }, i) => {
-                return (
-                  <li key={i}>
-                    <CMSLink {...link} />
-                  </li>
-                )
-              })}
-            </ul>
-          )}
+        {showRichText && richText && (
+          <Box>
+            <RichText
+              className="text-white text-2xl text-[#FDFFFC] md:text-3xl font-semibold"
+              data={richText}
+              enableGutter={false}
+            />
+          </Box>
+        )}
+        {Array.isArray(links) && links.length > 0 && (
+          <ul className="flex flex-wrap justify-center gap-4">
+            {links.map(({ link }, i) => (
+              <li key={i}>
+                <CMSLink {...link} />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
       <div className="min-h-[80vh] select-none">
         {media && typeof media === 'object' && (
